d2d6f59ba7a20a7131b86121bc1f55d60c1a5717
Fausto Schwantz <faustoschwantz@gmail.com>
2022-01-07 22:32:25 -0300

    chore: add open API config

--- src/application/main/main.ts
+++ src/application/main/main.ts
@@ -5,6 +5,7 @@ import * as helmet from 'helmet';
 
 import { port } from 'src/infraestructure/config/app.config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -12,9 +13,18 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Base NestJS API')
+    .setDescription('A Base NestJS API')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.use(helmet);
-
   await app.listen(port);
 }
 bootstrap();
