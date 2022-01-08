bb97f24ccd7a4272120acf80badd4cf0618d0e6c
Fausto Schwantz <faustoschwantz@gmail.com>
2022-01-08 11:31:43 -0300

    refactor: add tag an return in example controller

--- src/application/example/example.controller.ts
+++ src/application/example/example.controller.ts
@@ -1,7 +1,7 @@
 import { CreateExampleDto } from './dto/create-example.dto';
 import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ICreateExampleService } from 'src/domain/interfaces/services/create-example-service.interface';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Examples')
 @Controller('example')
@@ -12,6 +12,7 @@ export class ExampleController {
   ) {}
 
   @Post()
+  @ApiCreatedResponse({ description: 'Example created' })
   create(@Body() createExampleDto: CreateExampleDto) {
     return this.createExampleService.execute(createExampleDto);
   }
