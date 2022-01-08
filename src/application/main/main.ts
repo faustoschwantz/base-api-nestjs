f2ec19dc04b400914af81b7481c99a83c0119b61
Fausto Schwantz <faustoschwantz@gmail.com>
2022-01-07 21:59:12 -0300

    chore: add class validator pipe

--- src/application/main/main.ts
+++ src/application/main/main.ts
@@ -4,9 +4,12 @@ import { AppModule } from './app.module';
 import * as helmet from 'helmet';
 
 import { port } from 'src/infraestructure/config/app.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   app.use(helmet);
 
