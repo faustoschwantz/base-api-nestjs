2bfa63109b7d0ecee856fad686694918f4319995
Fausto Schwantz <faustoschwantz@gmail.com>
2022-01-07 21:50:12 -0300

    chore: add dotenv config

--- src/application/main/main.ts
+++ src/application/main/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 
 import * as helmet from 'helmet';
 
+import { port } from 'src/infraestructure/config/app.config';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet);
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
