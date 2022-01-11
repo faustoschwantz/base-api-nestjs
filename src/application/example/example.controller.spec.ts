5566b8cf7fa83505c17fb52536c8dc7ac7d9faa6
Fausto Schwantz <faustoschwantz@gmail.com>
2022-01-11 09:18:39 -0300

    test: added test on example creation controller

--- src/application/example/example.controller.spec.ts
+++ src/application/example/example.controller.spec.ts
@@ -1,9 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ICreateExampleService } from 'src/domain/interfaces/services/create-example-service.interface';
+import { IDeleteExampleService } from 'src/domain/interfaces/services/delete-example-service.interface';
+import { IFindAllExampleService } from 'src/domain/interfaces/services/find-all-example-service.interface';
+import { IFindExampleService } from 'src/domain/interfaces/services/find-example-service.interface';
+import { IUpdateExampleService } from 'src/domain/interfaces/services/update-example-service.interface';
+import { CreateExampleDto } from './dto/create-example.dto';
+import { ExampleDto } from './dto/example.dto';
 import { ExampleController } from './example.controller';
+
+const exampleFake: ExampleDto = {
+  id: 'any-id',
+  description: 'any-description',
+  enable: false,
+};
+
+const createExampleFake: CreateExampleDto = {
+  description: 'any-description',
+  enable: false,
+};
 
 describe('ExampleController', () => {
   let controller: ExampleController;
+  let createExampleServiceStub: ICreateExampleService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,6 +30,30 @@ describe('ExampleController', () => {
         {
           provide: 'ICreateExampleService',
           useFactory: (): ICreateExampleService => ({
+            execute: jest.fn(async () => exampleFake),
+          }),
+        },
+        {
+          provide: 'IUpdateExampleService',
+          useFactory: (): IUpdateExampleService => ({
+            execute: jest.fn(),
+          }),
+        },
+        {
+          provide: 'IFindExampleService',
+          useFactory: (): IFindExampleService => ({
+            execute: jest.fn(),
+          }),
+        },
+        {
+          provide: 'IFindAllExampleService',
+          useFactory: (): IFindAllExampleService => ({
+            execute: jest.fn(),
+          }),
+        },
+        {
+          provide: 'IDeleteExampleService',
+          useFactory: (): IDeleteExampleService => ({
             execute: jest.fn(),
           }),
         },
@@ -19,9 +61,27 @@ describe('ExampleController', () => {
     }).compile();
 
     controller = module.get<ExampleController>(ExampleController);
+    createExampleServiceStub = module.get<ICreateExampleService>(
+      'ICreateExampleService',
+    );
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(createExampleServiceStub).toBeDefined();
+  });
+
+  describe('When to create a Example', () => {
+    it('Should be create and return a Example', async () => {
+      const createExampleServiceSpy = jest.spyOn(
+        createExampleServiceStub,
+        'execute',
+      );
+
+      const response = await controller.create(createExampleFake);
+
+      expect(createExampleServiceSpy).toHaveBeenCalledWith(createExampleFake);
+      expect(response).toEqual(exampleFake);
+    });
   });
 });
