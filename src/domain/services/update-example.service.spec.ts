a5da0a3c6c1cece729382a335f392496a1b5fc48
Fausto Schwantz <faustoschwantz@gmail.com>
2022-01-11 10:34:35 -0300

    test: added test on example update service

--- src/domain/services/update-example.service.spec.ts
+++ src/domain/services/update-example.service.spec.ts
@@ -1,31 +1,82 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { ExampleDto } from 'src/application/example/dto/example.dto';
+import { UpdateExampleDto } from 'src/application/example/dto/update-example.dto';
+import { ExampleDocument } from 'src/infrastructure/mongoose/schemas/example.schema';
 import { IExampleMongooseRepository } from '../interfaces/repositories/example-mongoose-repository.interface';
-import { ICreateExampleService } from '../interfaces/services/create-example-service.interface';
-import { CreateExampleService } from './create-example.service';
+import { IUpdateExampleService } from '../interfaces/services/update-example-service.interface';
+import { UpdateExampleService } from './update-example.service';
 
-describe('CreateExampleService', () => {
-  let service: ICreateExampleService;
+const exampleFake: ExampleDto = {
+  id: 'any-id',
+  description: 'any-description',
+  enable: false,
+};
+
+const updateExampleFake: UpdateExampleDto = {
+  description: 'any-description',
+  enable: false,
+};
+
+describe('UpdateExampleService', () => {
+  let service: IUpdateExampleService;
+  let repository: IExampleMongooseRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         {
-          provide: 'ICreateExampleService',
-          useClass: CreateExampleService,
+          provide: 'IUpdateExampleService',
+          useClass: UpdateExampleService,
         },
         {
           provide: 'IExampleMongooseRepository',
           useFactory: (): Partial<IExampleMongooseRepository> => ({
-            create: jest.fn(),
+            update: jest.fn(async () => exampleFake as ExampleDocument),
           }),
         },
       ],
     }).compile();
 
-    service = module.get<ICreateExampleService>('ICreateExampleService');
+    service = module.get<IUpdateExampleService>('IUpdateExampleService');
+    repository = module.get<IExampleMongooseRepository>(
+      'IExampleMongooseRepository',
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(repository).toBeDefined();
+  });
+
+  describe('When to execute the method', () => {
+    it('Should be return a NotFoundException if the example does not exist', async () => {
+      const repositoryUpdateSpy = jest
+        .spyOn(repository, 'update')
+        .mockReturnValueOnce(null);
+
+      const responsePromise = service.execute(
+        exampleFake.id,
+        updateExampleFake,
+      );
+
+      await expect(responsePromise).rejects.toThrow(NotFoundException);
+      expect(repositoryUpdateSpy).toHaveBeenCalledWith(
+        exampleFake.id,
+        updateExampleFake,
+      );
+    });
+
+    it('Should be update and return a Example', async () => {
+      const repositoryUpdateSpy = jest.spyOn(repository, 'update');
+
+      const response = await service.execute(exampleFake.id, updateExampleFake);
+
+      expect(repositoryUpdateSpy).toHaveBeenCalledWith(
+        exampleFake.id,
+        updateExampleFake,
+      );
+      expect(response).toBeUndefined();
+    });
   });
 });
